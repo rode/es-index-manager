02b49dcec92787e055f9f8be5e4c2b862594ac34
Alex Ashley <alexa@liatrio.com>
2021-05-13 17:46:24 -0500

    migration orchestrator tests

--- indexmanager/orchestrator.go
+++ indexmanager/orchestrator.go
@@ -40,19 +40,19 @@ func NewMigrationOrchestrator(logger *zap.Logger, migrator Migrator) MigrationOr
 
 func (m *migrationOrchestrator) RunMigrations(ctx context.Context) error {
 	log := m.logger.Named("RunMigrations")
-	migrationsToRun, err := m.migrator.GetMigrations(ctx)
+	migrations, err := m.migrator.GetMigrations(ctx)
 	if err != nil {
 		return err
 	}
 
-	if len(migrationsToRun) == 0 {
+	if len(migrations) == 0 {
 		log.Info("No migrations to run")
 		return nil
 	}
 
-	log.Info(fmt.Sprintf("Discovered %d migrations to run", len(migrationsToRun)))
+	log.Info(fmt.Sprintf("Discovered %d migrations to run", len(migrations)))
 
-	for _, migration := range migrationsToRun {
+	for _, migration := range migrations {
 		if err := m.migrator.Migrate(ctx, migration); err != nil {
 			return err
 		}
